a20fe6d8823e2e13f96fda782485155dcbe6cc69
Delwyn de Villiers <delwyn.d@gmail.com>
2014-03-21 16:05:39 +0000

    show message when no photos match filters

--- test/e2e/photos.js
+++ test/e2e/photos.js
@@ -8,16 +8,16 @@ describe('Photos', function() {
 
   // Need to figure out how to mock out factories???
 
-  it("should list all photos", function() {
+  it('should list all photos', function() {
     expect(repeater('.photos li.photo').count()).toBe(8);
   });
 
-  it("should filter photos by query string", function() {
+  it('should filter photos by query string', function() {
     input('query').enter('london eye');
     expect(repeater('.photos li.photo').count()).toBe(1);
   });
 
-  it("should filter photos by line", function() {
+  it('should filter photos by line', function() {
     element('#line-filter-1').click();
     expect(repeater('.photos li.photo').count()).toBe(2);
   });
@@ -27,4 +27,17 @@ describe('Photos', function() {
     input('query').enter('Leake Street');
     expect(repeater('.photos li.photo').count()).toBe(1);
   });
+
+  describe('when none match', function() {
+    beforeEach(function() {
+      expect(repeater('.photos li.no-photos').count()).toBe(0);
+    });
+
+    it('should show no photos found', function() {
+      input('query').enter('zzz');
+      expect(repeater('.photos li.photo').count()).toBe(0);
+      expect(repeater('.photos li.no-photos').count()).toBe(1);
+    });
+  });
+
 });
